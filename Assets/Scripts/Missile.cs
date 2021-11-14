e4c19041aa5969b02741805f8df7cc4b59edc75e
Saboffzl <eagle_st@nurfuerspam.de>
2021-11-14 14:18:33 +0100

    INPUT ACTION MANAGER
    i hopfully will never forget this again!

--- Assets/Scripts/Missile.cs
+++ Assets/Scripts/Missile.cs
@@ -98,7 +98,7 @@ public class Missile : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log(collision.gameObject);
+        //Debug.Log(collision.gameObject);
         Explode();
     }
 
--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -12,7 +12,7 @@ public class WeaponController : MonoBehaviour
     ObjectPooler pooler;
     Transform muzzle;
 
-    int poolIndex = 0;
+    static int poolIndex = 0;
     float cooldown = 0;
     bool isA, wasA;
     bool isB, wasB;
@@ -41,12 +41,12 @@ public class WeaponController : MonoBehaviour
 
         foreach (InputDevice rightHand in rightHandedControllers)
         {
-            Debug.Log(rightHandedControllers.Count);
+            //Debug.Log(rightHandedControllers.Count);
 
             if (rightHand.TryGetFeatureValue(CommonUsages.triggerButton, out bool triggerValue) && triggerValue)
             {
                 //Debug.Log(string.Format("Device name '{0}' has characteristics '{1}'", rightHand.name, rightHand.characteristics.ToString()));
-                Debug.Log("Trigger button is pressed.");
+                //Debug.Log("Trigger button is pressed.");
 
                 if (cooldown <= .0f)
                 {
@@ -59,7 +59,7 @@ public class WeaponController : MonoBehaviour
             if (rightHand.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) && primaryButtonValue)
             {
                 //Debug.Log(string.Format("Device name '{0}' has characteristics '{1}'", rightHand.name, rightHand.characteristics.ToString()));
-                Debug.Log("A button is pressed.");
+                //Debug.Log("A button is pressed.");
 
                 isA = true;
                 if (isA != wasA)
@@ -76,7 +76,7 @@ public class WeaponController : MonoBehaviour
             if (rightHand.TryGetFeatureValue(CommonUsages.secondaryButton, out bool secondaryButtonValue) && secondaryButtonValue)
             {
                 //Debug.Log(string.Format("Device name '{0}' has characteristics '{1}'", rightHand.name, rightHand.characteristics.ToString()));
-                Debug.Log("B button is pressed.");
+                //Debug.Log("B button is pressed.");
 
                 isB = true;
                 if (isB != wasB)
@@ -94,6 +94,7 @@ public class WeaponController : MonoBehaviour
 
     public void NextButton()
     {
+        Debug.Log("Next button pressed");
         poolIndex++;
         if (poolIndex >= pooler.pools.Count)
         {
@@ -104,6 +105,7 @@ public class WeaponController : MonoBehaviour
 
     public void PreviousButton()
     {
+        Debug.Log("Prev button pressed");
         poolIndex--;
         if (poolIndex < 0)
         {
