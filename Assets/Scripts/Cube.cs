905a162551424327cc1f152a90c3fd6b33f6bace
Saboffzl <eagle_st@nurfuerspam.de>
2022-07-06 22:26:06 +0200

    new projectile

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball : MonoBehaviour
+public class Cube : MonoBehaviour
 {
     [SerializeField]
-    int projectileIndex = 1;
+    int projectileIndex = 2;
     Rigidbody rb;
     void OnEnable()
     {
@@ -13,5 +13,10 @@ public class Ball : MonoBehaviour
         rb.velocity = Vector3.zero;
         rb.AddForce(transform.forward * ProjectileManager.proManager.projectiles[projectileIndex].power);
         GetComponent<Renderer>().material.color = Random.ColorHSV(0f, 1f, 0.8f, 1f, 0.5f, 1f);
+        float rx = Random.value / 5f;
+        float ry = Random.value / 5f;
+        float rz = Random.value / 5f;
+        Vector3 randomScale = new Vector3(rx, ry, rz) + transform.localScale;
+        transform.localScale = randomScale;
     }
 }
